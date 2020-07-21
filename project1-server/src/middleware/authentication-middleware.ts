c8958f25d137119bcb43f1697916b8d0ee82c184
Arlette Inserto <you@example.com>
2020-07-21 16:31:10 -0700

    fixed role

--- project1-server/src/middleware/authentication-middleware.ts
+++ project1-server/src/middleware/authentication-middleware.ts
@@ -4,7 +4,7 @@ export function authenticationMiddleware(req:Request, res:Response, next:NextFun
     if(!req.session.user){
         res.status(401).send('Please Log in')
     }else{
-        console.log(`user ${req.session.user.username} has a role of ${req.session.user.role.role}`);
+        console.log(`user ${req.session.user.username} has a role of ${req.session.user.role}`);
         next()
     }
 }
