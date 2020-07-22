2248410c45208089824a9803ce2dcc860d69da15
Arlette Inserto <you@example.com>
2020-07-21 17:27:57 -0700

    cors

--- project1-server/src/middleware/cors-filter.ts
+++ project1-server/src/middleware/cors-filter.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction} from "express";
 
 export function corsFilter(req:Request, res:Response, next:NextFunction){
-    // res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)
-    res.header('Access-Control-Allow-Origin', `http://findmytutor.saivyl.com`)
+    res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)
+    // res.header('Access-Control-Allow-Origin', 'http://findmytutor.saivyl.com')
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization')
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
